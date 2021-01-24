e29d1bed58168cc7a8e0087c93ac75e58abec212
Jay Kothari <kotharijay65@gmail.com>
2021-01-24 20:34:22 +0530

    Firebase Setup

---
+++ app/src/main/java/com/example/woc30_android_chat_application_jay_kothari/Login.java
@@ -0,0 +1,14 @@
+package com.example.woc30_android_chat_application_jay_kothari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
