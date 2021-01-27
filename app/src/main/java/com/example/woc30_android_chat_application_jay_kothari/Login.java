20b0cf36ba9ad80e7a8f50b9f8c2d3f2e79351a3
Jay Kothari <kotharijay65@gmail.com>
2021-01-27 23:57:07 +0530

    Basic Sign Up Using Phone Number Done. TODO: Make UI better

--- app/src/main/java/com/example/woc30_android_chat_application_jay_kothari/Login.java
+++ app/src/main/java/com/example/woc30_android_chat_application_jay_kothari/Login.java
@@ -1,14 +1,147 @@
 package com.example.woc30_android_chat_application_jay_kothari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Login extends AppCompatActivity {
+
+    String mVerificationId;
+
+    //Variables for UI ellements
+    private EditText mPhoneNumber, mCode;
+    private Button mSendCode, mSignIn;
+
+    //Firebase Variables
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Initializing firebase
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        //Check if user is already signed in
+        userIsLoggedIn(FirebaseAuth.getInstance().getCurrentUser());
+
+        //Initializing private variables
+        //Edit Text Box
+        mPhoneNumber = findViewById(R.id.edTxtNumber);
+        mCode = findViewById(R.id.edTxtCode);
+
+        //Buttons
+        mSendCode = findViewById(R.id.btnSendCode);
+        mSignIn = findViewById(R.id.btnSignIn);
+
+
+        //Adding Event Listners
+        mSendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPhoneVerfication();
+            }
+        });
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mVerificationId != null) {
+                    verfifyPhoneNumber();
+                }
+            }
+        });
+
+        mCallBacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(verificationId, forceResendingToken);
+                mVerificationId = verificationId;
+            }
+        };
+    }
+
+    private void verfifyPhoneNumber(){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, mCode.getText().toString());
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //Login Complete redirect
+                    FirebaseUser user = task.getResult().getUser();
+                    userIsLoggedIn(user);
+                } else {
+                    if(task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                        //Invalid Code
+                        Toast.makeText(Login.this, "Invalid Code", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void userIsLoggedIn(FirebaseUser user) {
+        if(user != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+            return;
+        }
+    }
+
+    private void startPhoneVerfication(){
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(mPhoneNumber.getText().toString())       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallBacks
+        );
     }
 }
---
+++ app/src/main/java/com/example/woc30_android_chat_application_jay_kothari/UserListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.woc30_android_chat_application_jay_kothari;
+
+public class UserListAdapter {
+}
